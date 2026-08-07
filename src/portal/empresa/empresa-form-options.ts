f16d8068faea658dce311efd6d95068a572b562f
DylanDrouet <dylan.drouet@uees.edu.ec>
2026-08-07 09:54:22 -0500

    feat: agregar opciones compartidas para los formularios de empresa

---
+++ src/portal/empresa/empresa-form-options.ts
@@ -0,0 +1,47 @@
+export const TIPO_CONTRIBUYENTE_OPTIONS = ['Persona Natural', 'Persona Jurídica'] as const
+
+export const REGIMEN_TRIBUTARIO_OPTIONS = [
+  'RIMPE Emprendedor',
+  'RIMPE Negocio Popular',
+  'Régimen General',
+] as const
+
+export const ACTIVIDAD_ECONOMICA_OPTIONS = [
+  'C1410 - Fabricación de prendas de vestir',
+  'G4711 - Venta al por menor en comercios no especializados',
+  'G4620 - Venta al por mayor de materias primas agropecuarias',
+  'I5610 - Actividades de restaurantes y servicio móvil de comidas',
+  'J6202 - Consultoría informática y actividades relacionadas',
+  'M6920 - Actividades de contabilidad y auditoría',
+  'F4100 - Construcción de edificios',
+  'H4923 - Transporte de carga por carretera',
+] as const
+
+export const PROVINCIA_OPTIONS = [
+  'Azuay',
+  'Bolívar',
+  'Cañar',
+  'Carchi',
+  'Chimborazo',
+  'Cotopaxi',
+  'El Oro',
+  'Esmeraldas',
+  'Galápagos',
+  'Guayas',
+  'Imbabura',
+  'Loja',
+  'Los Ríos',
+  'Manabí',
+  'Morona Santiago',
+  'Napo',
+  'Orellana',
+  'Pastaza',
+  'Pichincha',
+  'Santa Elena',
+  'Santo Domingo de los Tsáchilas',
+  'Sucumbíos',
+  'Tungurahua',
+  'Zamora Chinchipe',
+] as const
+
+export const SI_NO_OPTIONS = ['Sí', 'No'] as const
